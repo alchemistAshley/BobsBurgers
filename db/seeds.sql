e929360dd314a204d8a614fcb9ebc3a0b706c345
Ashley Winnop <arwinnop@gmail.com>
2018-06-07 09:20:50 -0600

    styling

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, order_up)
+VALUES ('Baby, you can chive my car', false),
+  ('I Know Why the Cajun Burger Sings', false),
+  ('A Good Manchego Is Hard to Find Burger', true),
+  ('Kale Mary Burger', false),
+  ('Beets Me Burger', false),
+  ('Bleu by You Burger', false);
