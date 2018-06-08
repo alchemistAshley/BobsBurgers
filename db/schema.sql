7b2f39ae969da96c9858996e281891dea5c872ec
Ashley Winnop <arwinnop@gmail.com>
2018-06-07 23:13:22 -0600

    kale marygit add .

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
   id INTEGER AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100) NOT NULL,
-  order_up BOOLEAN NOT NULL,
+  burger_name VARCHAR(200) NOT NULL,
+  order_up BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
 INSERT INTO burgers (burger_name, order_up)
-VALUES ('Baby, you can chive my car', false),
-  ('I Know Why the Cajun Burger Sings', false),
-  ('A Good Manchego Is Hard to Find Burger', true),
+VALUES ('Baby, You Can Chive My Car', false),
+  ('I Know Why The Cajun Burger Sings', false),
+  ('A Good Manchego Is Hard to Find Burger', false),
   ('Kale Mary Burger', false),
   ('Beets Me Burger', false),
-  ('Bleu by You Burger', false);
+  ('Bleu By You Burger', false),
+  ('Curry On My Wayward Bun', false),
+  ('If You"ve Got It, Croissant It', false);
